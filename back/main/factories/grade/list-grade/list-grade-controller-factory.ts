4a88b00f746d12101a62cce295dc2839d03db0df
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-08-07 18:13:58 -0300

    feat: Implementada rota get:'/grades' para listagem de grades

---
+++ back/main/factories/grade/list-grade/list-grade-controller-factory.ts
@@ -0,0 +1,12 @@
+import { Controller } from "../../../../presentation/protocols";
+import { ListGradeController } from "../../../../presentation/controllers/grade/list-grade/list-grade-controller";
+
+import { ListGradeUseCase } from "../../../../domain/usecases/grade/list/list-grade-usecase";
+
+import { GradeRepository } from "../../../../data/database/postgres/grade/grade-repository";
+
+export const makeListGradeController = (): Controller => {
+  const gradeRepository = new GradeRepository();
+  const listGradeUseCase = new ListGradeUseCase(gradeRepository);
+  return new ListGradeController(listGradeUseCase);
+}
--- back/main/routes/grade-routes.ts
+++ back/main/routes/grade-routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express'
+
 import { adaptRoute } from '../adapters/express/express-route-adapter'
+
 import { makeAddGradeController } from '../factories/grade/add-grade/add-grade-controller-factory'
+import { makeListGradeController } from '../factories/grade/list-grade/list-grade-controller-factory';
 
 export default (router: Router): void => {
   router.post('/grades', adaptRoute(makeAddGradeController()));
+  router.get('/grades', adaptRoute(makeListGradeController()));
 }
