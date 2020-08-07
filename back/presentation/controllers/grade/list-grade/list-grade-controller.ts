58db538fb548f047cd4f0967700416791cbebc96
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-08-07 16:18:46 -0300

    feat: implementado o controller 'list-grade-controller'

--- back/presentation/controllers/grade/list-grade/list-grade-controller.ts
+++ back/presentation/controllers/grade/list-grade/list-grade-controller.ts
@@ -1,24 +1,21 @@
-import { AddGrade } from "../../../../domain/usecases/grade/add/add-grade";
+import { ListGrade } from "../../../../domain/usecases/grade/list/list-grade";
 
 import { Controller } from "../../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../../protocols/http";
 import { ok, serverError } from "../../../helpers/http";
 
-export class AddGradeController implements Controller {
+export class ListGradeController implements Controller {
   constructor (
-    private readonly addGrade: AddGrade
+    private readonly listGrade: ListGrade
   ) {
-    this.addGrade = addGrade;
+    this.listGrade = listGrade;
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { nome, descricao } = httpRequest.body;
 
-      const grade = await this.addGrade.add({
-        nome,
-        descricao
-      });
+      const grade = await this.listGrade.list();
 
       return ok(grade);
     } catch (error) {
