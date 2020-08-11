0179ad42be8d61e2ba51cef96e3c23ae44ca3c74
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-08-11 16:58:26 -0300

    feat (front): implementada a listagem e seleção de grade para o 'grade.component'

--- front/src/app/pages/grade/grade.component.ts
+++ front/src/app/pages/grade/grade.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { GradeService } from 'app/shared/services/grade.service';
+import { ColumnDatatable } from 'app/shared/models/columnDatatable';
+import { Grade } from 'app/shared/models/grade.model';
 
 @Component({
-    selector: 'grade-cmp',
-    templateUrl: 'grade.component.html'
+  selector: 'grade-cmp',
+  templateUrl: 'grade.component.html'
 })
 
 export class GradeComponent implements OnInit{
 
-    ngOnInit(){
-    }
+  rows: Grade[];
+  columns: ColumnDatatable[] = [
+    {name: 'ID', prop: 'id', maxWidth: 100},
+    {name: 'Nome', prop: 'nome'}
+  ];
+
+  constructor(private gradeService: GradeService) {}
+
+  ngOnInit(){
+    this.gradeService.list().subscribe(result => {
+      this.rows = result;
+    });
+  }
+
+  doubleClick(grade: Grade) {
+    this.gradeService.setIdGradeOnStorage(grade.id);
+  }
 }
