ec9e1e04e5ae00c833291aa1d875b854fd5e0b00
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-08-11 15:06:23 -0300

    feat (front): implementado o interceptor 'http-error'

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ToastrModule } from "ngx-toastr";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { SharedModule} from 'app/shared/shared.module';
 
@@ -10,6 +11,7 @@ import { AppRoutes } from './app.routing';
 import { GradeGuard } from './shared/guards/grade-guard.service'
 
 import { MainLayoutComponent } from './layouts/main/main-layout.component';
+import { HttpErrorInterceptor } from './shared/interceptors/http-error.interceptor';
 
 
 @NgModule({
@@ -19,11 +21,19 @@ import { MainLayoutComponent } from './layouts/main/main-layout.component';
   ],
   imports: [
     BrowserAnimationsModule,
+    HttpClientModule,
     RouterModule.forRoot(AppRoutes),
     ToastrModule.forRoot(),
     SharedModule,
   ],
-  providers: [GradeGuard],
+  providers: [
+    GradeGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ front/src/app/shared/interceptors/http-error.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+export class HttpErrorInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if(error.status === 500) {
+          alert("Ocorreu um erro no servidor, por favor tente novamente")
+        }
+        return throwError(error)
+      })
+    )
+  }
+ }
