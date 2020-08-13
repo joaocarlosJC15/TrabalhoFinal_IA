7e4a8b0d987079ef0113466c4ecb68805363beae
João Carlos Alvim <jc15joaocarlos@gmail.com>
2020-08-13 00:00:52 -0300

    fix (back): corrigido cors

--- back/src/main/middlewares/cors.ts
+++ back/src/main/middlewares/cors.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 export const cors = (request: Request, response: Response, next: NextFunction): void => {
   response.set('Access-Control-Allow-Origin', '*');
-  response.set('Acess-Control-Allow-Methods', '*');
-  response.set('Acess-Control-Allow-Headers', '*');
+  response.set('Access-Control-Allow-Methods', '*');
+  response.set('Access-Control-Allow-Headers', '*');
   next();
 }
